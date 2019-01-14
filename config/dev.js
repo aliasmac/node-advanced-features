03fe111b736dd233c9dea8da1aa495ecc05020d1
thexyman <makdaa1@gmail.com>
2019-01-14 20:37:56 +0000

    started work on redis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://ali:password1@ds157064.mlab.com:57064/blog_dev',
   cookieKey: '123123123'
 };
