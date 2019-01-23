2d198619a917a0e1f711dcb35a805ac7d434bcfb
thexyman <makdaa1@gmail.com>
2019-01-23 21:12:03 +0000

    added redis boilerplate code

---
+++ services/cache.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+client.get = util.promisify(client.get)
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.exec = async function () {
+
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }))
+
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key)
+
+
+  // If we do, return that
+  if (cacheValue) {
+    console.log(cacheValue)
+    // return res.send(JSON.parse(cachedBlogs))
+  }
+
+  // Otherwise issue the query and store the result in redis
+  const result = await exec.apply(this, arguments)
+  console.log(result)
+}
