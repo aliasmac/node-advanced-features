62c626a8de2583b2ab878f8f55111d6867a556cf
thexyman <makdaa1@gmail.com>
2019-01-24 19:12:40 +0000

    implemented Redis and clear cache middleware

--- services/cache.js
+++ services/cache.js
@@ -4,26 +4,57 @@ const util = require('util')
 
 const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec
 
+mongoose.Query.prototype.cache = function(options = {}) {
+  // creates a property on the query object which we can reference in 
+  // function below
+  this.useCache = true
+  this.hashKey = JSON.stringify(options.key || '')
+  // return this, return the query object
+  return this
+}
+
 mongoose.Query.prototype.exec = async function () {
+
+  if (!this.useCache) {
+    console.log("MONGODB")
+    return exec.apply(this, arguments)
+  }
 
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
   }))
 
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key)
+  const cacheValue = await client.hget(this.hashKey, key)
 
 
   // If we do, return that
   if (cacheValue) {
-    console.log(cacheValue)
-    // return res.send(JSON.parse(cachedBlogs))
+    console.log("REDIS STORE")
+    const doc = JSON.parse(cacheValue)
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc)
+
   }
 
   // Otherwise issue the query and store the result in redis
   const result = await exec.apply(this, arguments)
-  console.log(result)
+
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 5)
+
+  // console.log(result) // returns a mongoose document - or model instance 
+
+  return result 
+  
+}
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey))
+  }
 }
